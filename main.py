10114115737719166a03517b77f6297133d31889
zekad <zekadorofeev9@gmail.com>
2022-10-13 00:47:21 +0500

    add connection postgres

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import uvicorn
+import asyncio
 from fastapi import FastAPI
 from routing import create_routing
-
+from src.telegram.items.modules import set_webhook
 
 app = FastAPI()
 
 create_routing(app)
 
 if __name__ == "__main__":
+    #loop = asyncio.get_event_loop()
+    #loop.run_until_complete(set_webhook())
+
     uvicorn.run(app, host="0.0.0.0", port=80)
--- src/telegram/callback.py
+++ src/telegram/callback.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Depends
+from sqlalchemy.future import select
+from src.telegram.common.db.models.user import User
+from src.telegram.common.db.base import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
 from src.telegram.schemas.messages import MessageBodyModel
 
 telegram = FastAPI()
 
 
 @telegram.post("/webhook")
-async def callback(event: MessageBodyModel):
-    print(event)
+async def callback(event: MessageBodyModel, db: AsyncSession = Depends(get_db)):
+
+    result = await db.execute(select(User))
+    item = result.scalars().all()
+
+    print(item)
+
